04d5ccce8eda8e8023e214bdc49c1887140875cb
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2012-07-13 11:16:26 +0100

    Fix puppet URL in ssh

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -9,7 +9,7 @@ class ssh {
   file { '/etc/ssh/sshd_config':
     ensure => file,
     mode => 600,
-    source => 'puppet://modules/ssh/sshd_config',
+    source => 'puppet:///modules/ssh/sshd_config',
   }
 
   service { 'sshd':
