c22e470dea8067187bb3983b09221ca25f618b7e
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2012-07-13 11:14:16 +0100

    Initial import

---
+++ modules/ssh/manifests/init.pp
@@ -0,0 +1,22 @@
+# Manage our ssh server
+
+class ssh {
+  package { 'openssh-server':
+    ensure => present,
+     before => File['/etc/ssh/sshd_config'],
+  }
+
+  file { '/etc/ssh/sshd_config':
+    ensure => file,
+    mode => 600,
+    source => 'puppet://modules/ssh/sshd_config',
+  }
+
+  service { 'sshd':
+     ensure => running,
+     enable => true,
+     hasrestart => true,
+     hasstatus => true,
+     subscribe => File['/etc/ssh/sshd_config'],
+  }
+}
