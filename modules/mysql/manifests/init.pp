37feb6d2ac388e16ba5082cfe1deba5d7570af97
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-03-01 12:30:48 +0000

    Perform a database dump of MySQL every day.
    Keeping dumps for 7 days.

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -16,10 +16,39 @@ class mysql {
 			name => ['mysql-server', 'mysql-client'],
 			ensure => present,
 	}
+
 	service { 'mysql' :
 			ensure => running,
 			enable => true,
 			require => Package["mysql"],
 	}
+	
+	# We do not want anyone looking at the backups
+	file { '/var/backups/mysql' :
+		ensure => "directory",
+        owner => root,
+        group => root,
+        mode => 0700,
+    }
+	
+	# Do daily backups of all databases
+	file { '/etc/cron.daily/mysqlbackup' :
+		source => 'puppet:///modules/mysql/mysqlbackup-cron',
+        owner => root,
+        group => root,
+        mode => 0755,
+        require => [
+        	File['/var/backups/mysql'],
+        	Package['mysql'],
+        ],
+    }
+    
+    file { '/etc/logrotate.d/mysqlbackup' :
+    	source => 'puppet:///modules/mysql/mysqlbackup-logrotate',
+        owner => root,
+        group => root,
+        mode => 0644,
+        require => File['/var/backups/mysql'],
+    }
 }
 
