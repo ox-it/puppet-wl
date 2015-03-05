9917d7e1f884e8ee44937aff186fc586e3b3feef
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2015-03-05 14:56:51 +0000

    Generate SSL certificate for apache.

--- services/svc_logstash/test/start.sh
+++ services/svc_logstash/test/start.sh
@@ -13,7 +13,9 @@ if [ ! -f /etc/rabbitmq/cert.pem -o ! -f /etc/rabbitmq/key.pem -o ! -f /etc/rabb
   openssl req -x509 -subj '/CN=localhost' -nodes -newkey rsa:2048 -keyout /etc/rabbitmq/key.pem -out /etc/rabbitmq/cert.pem -days 365
   cp /etc/rabbitmq/cert.pem /etc/rabbitmq/cacert.pem
 fi
-
+if [ ! -f /etc/ssl/certs/localhost.crt -o ! -f /etc/ssl/private/localhost.key ] ; then
+  openssl req -x509 -subj '/CN=localhost' -nodes -newkey rsa:2048 -keyout /etc/ssl/private/localhost.key -out /etc/ssl/certs/localhost.crt -days 365
+fi
 
 if [ $(find /var/cache/apt -maxdepth 0 -mtime -24| wc -l) -eq 0 ]; then
   apt-get update
