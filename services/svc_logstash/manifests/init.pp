ad272d05afa78bb22fe6af7f2bca964f9d0ebdf7
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2015-03-05 10:19:49 +0000

    Log stash closer to working.
    Each of the extra services runs on their own loopback interface. This means they don’t clash over ports and discovery works well. puppet has good setup of adding names for the extra hosts.

--- services/svc_logstash/manifests/init.pp
+++ services/svc_logstash/manifests/init.pp
@@ -5,6 +5,50 @@ class svc_logstash (
   ){
 
   include apache2
+  include secrets
+  
+  # This is the password used to connect logstash to rabbitmq
+  # It shouldn't be used for client connections to rabbitmq
+  $rabbitmq_username = "logstash"
+  $rabbitmq_password = sha1("${fqdn}${secrets::secret}rabbitmq")
+
+  exec {'ifup lo:2':
+    command => '/sbin/ifup lo:2',
+    refreshonly => true,
+  }
+    
+  exec {'ifup lo:3':
+    command => '/sbin/ifup lo:3',
+    refreshonly => true,
+  }
+    
+
+  file {'/etc/network/interfaces.d/lo2.cfg':
+    owner => root,
+    group => root,
+    mode => 0644,
+    source => 'puppet:///modules/svc_logstash/lo2.cfg',
+    notify => Exec['ifup lo:2'],
+  }
+
+  file {'/etc/network/interfaces.d/lo3.cfg':
+    owner => root,
+    group => root,
+    mode => 0644,
+    source => 'puppet:///modules/svc_logstash/lo3.cfg',
+    notify => Exec['ifup lo:3'],
+  }
+
+
+  host {'logstash':
+    ip => '127.0.0.2',
+  }
+
+  host {'elasticsearch':
+   ip => '127.0.0.3',
+  }
+
+  
 
   apt::key {'D88E42B4':
     ensure => present,
@@ -36,25 +80,44 @@ class svc_logstash (
     require => Package["apache2"], 
   } ->
 
-  apache2::module { "webauth":
-    ensure => "present",
-  } ->
-
-  apache2::modules { "ssl":
+  apache2::module { "ssl":
     ensure => "present",
     # Our custom SSL config
     require => File["/etc/apache2/mods-available/ssl.conf"],
-  } ->
+  } 
+  
+#  apache2::module { "webauth":
+#    ensure => "present",
+#    require => Package["webauth"],
+#  } ->
+#
+#  package { 'webauth':
+#    name => "libapache2-webauth",
+#    ensure => "present"
+#  }
 
   class { 'rabbitmq':
   } ->
 
+  rabbitmq::user { "$rabbitmq_username": 
+    password => $rabbitmq_password
+  } ->
+
   class { 'elasticsearch':
-    ensure => "present",
+  } ->
+
+  elasticsearch::instance { 'logstash':
+    config => {
+      'network.host' => 'elasticsearch'
+    },
   } ->
 
   class {'logstash': 
     java_install => true,
+  }
+
+  logstash::configfile { 'indexer':
+    content => template('svc_logstash/logstash/indexer.erb'),
   }
 
 }
